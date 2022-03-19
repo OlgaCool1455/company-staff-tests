699cf230dda30648cd2bce99225d448e47da1f23
DimaEff <69797190+DimaEff@users.noreply.github.com>
2022-03-19 17:04:15 +0300

    init app and firebase config

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="js">
+    import router from "page";
+    import Index from "./pages/index.svelte";
+
+    let page;
+    let params = {};
+
+    router("/", () => page = Index)
+
+    router.start();
+</script>
+
+<svelte:component this={page} {params}/>
